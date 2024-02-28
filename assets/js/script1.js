fb5d868a92d843d021146ad1224ae0c96380637c
MoeFingers <mbzuiter@gmail.com>
2024-02-27 22:49:19 -0700

    finished outline to view all key/values when expanding a given truck

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -24,7 +24,7 @@ document.getElementById("truckCardsContainer").innerHTML = "";
         ${
         trucks[truckKey].usedFor
             ? trucks[truckKey].usedFor
-                .map((usedFor) => `<li>${usedFor}</li>`)
+                .map((usedFor) => `<li class="badge">${usedFor}</li>`)
                 .join("")
             : ""
         }
@@ -37,7 +37,7 @@ document.getElementById("truckCardsContainer").innerHTML = "";
     ${
         trucks[truckKey].preTrip
             ? `<h2>Pre Trip</h2>
-            
+            <div class="preTripSection">
             ${
                 Object.keys(trucks[truckKey].preTrip)
                 .filter((preTripKey) => {return preTripKey != "lights"})
@@ -49,14 +49,88 @@ document.getElementById("truckCardsContainer").innerHTML = "";
                         : ""
                     }` +
                     Object.keys(trucks[truckKey].preTrip[preTripKey])
-                    .filter((preTripKey) => {return preTripKey != "name" && preTripKey != "images"})
+                    .filter((preTripKey) => {return preTripKey != "name" && preTripKey != "images" })
                     .map((preTripItemKey) =>
                         `${trucks[truckKey].preTrip[preTripKey][preTripItemKey]}`)
                     .join("|")
                     )
                     
+                .join("<br>") + 
+                `<div class="lightsSection">
+                <strong>Lights</strong>
+                <br>
+                ${
+                    Object.keys(trucks[truckKey].preTrip.lights)
+                    .filter((lightsKey) => {return lightsKey != "name"})
+                    .map((lightsItemKey) =>
+                        `${
+                            trucks[truckKey].preTrip.lights[lightsItemKey].name
+                            + trucks[truckKey].preTrip.lights[lightsItemKey].images.map((image) => `<img src="${image}">`).join("")
+                            + trucks[truckKey].preTrip.lights[lightsItemKey].notes
+                        }`
+                    ).join("<br>")
+
+                }
+                <div>`
+            }
+            </div>
+            `
+            : ""
+    }
+    ${
+        trucks[truckKey].postTrip
+            ? `<h2>Post Trip</h2>
+            <div class="postTripSection">
+            ${
+                Object.keys(trucks[truckKey].postTrip)
+                .filter((postTripKey) => {return postTripKey != "lights"})
+                .map((postTripKey) => 
+                    `<strong>${trucks[truckKey].postTrip[postTripKey].name}</strong>` +
+                    `${
+                        trucks[truckKey].postTrip[postTripKey].images
+                        ?  trucks[truckKey].postTrip[postTripKey].images.map((image) => `<img src="${image}">`).join("")
+                        : ""
+                    }` +
+                    Object.keys(trucks[truckKey].postTrip[postTripKey])
+                    .filter((postTripKey) => {return postTripKey != "name" && postTripKey != "images"})
+                    .map((postTripItemKey) =>
+                        `${trucks[truckKey].postTrip[postTripKey][postTripItemKey]}`)
+                    .join("|")
+                    )
+                    
                 .join("<br>")
             }
+            </div>
+            `
+            : ""
+    }
+    ${
+        trucks[truckKey].quirks
+            ? `<h2>Quirks</h2>
+            <div class="quirksSection">
+            ${
+                Object.keys(trucks[truckKey].quirks)
+                .map((quirksKey) => 
+                    `<strong>${trucks[truckKey].quirks[quirksKey].name}</strong>` +
+                    `${
+                        trucks[truckKey].quirks[quirksKey].images
+                        ?  trucks[truckKey].quirks[quirksKey].images.map((image) => `<img src="${image}">`).join("")
+                        : ""
+                    }` +
+                    Object.keys(trucks[truckKey].quirks[quirksKey])
+                    .filter((quirksKey) => {return quirksKey != "name" && quirksKey != "images" && quirksKey != "tags" })
+                    .map((quirksItemKey) =>
+                        `${trucks[truckKey].quirks[quirksKey][quirksItemKey]}`)
+                    .join("|")  + "<br>" +
+                    `${trucks[truckKey].quirks[quirksKey].tags.includes("operation") ? "<li class='quirkTag'>Affects <strong>operation</strong>.</li>" : ""}` +
+                    `${trucks[truckKey].quirks[quirksKey].tags.includes("prevention") ? "<li class='quirkTag'>Prevents <strong>damage</strong> to equipment.</li>" : ""}` +
+                    `${trucks[truckKey].quirks[quirksKey].tags.includes("safety") ? "<li class='quirkTag'><strong>Safety Warning</strong></li>" : ""}`
+
+                    )
+                    
+                .join("<br>")
+            }
+            </div>
             `
             : ""
     }
